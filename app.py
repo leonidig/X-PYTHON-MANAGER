75fa4993806c3441d9bb0cad2c25c67c93b22f82
leonidig <leonid200955@gmail.com>
2024-08-04 11:38:46 +0300

    info page & styles

--- app.py
+++ app.py
@@ -15,5 +15,20 @@ def index():
     return render_template("index.html", **balances)
 
 
+
+@app.get("/balance/<int:balance_id>")
+def get_balance_info(balance_id):
+    balances = get(f"{BACKEND_URL}/get_balances").json()
+    selected_balance = None
+    for item in balances:
+        if item.get('id')== balance_id:
+            selected_balance = item
+            break
+    if selected_balance:
+        return render_template("info.html", balance=selected_balance)
+    else:
+        return f"Could not found balance with id : {item}"
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- balance.py
+++ balance.py
@@ -10,9 +10,19 @@ app = FastAPI()
 @app.get("/get_balances")
 def get_balance():
     with Session.begin() as session:
-        balance = Balance(owner="Leonid", total=1298.7)
-        session.add(balance)
+        balance1 = Balance(owner="Leonid", total=1298.7)
+        balance2 = Balance(owner="John", total=457.2)
+        balance3 = Balance(owner="Patric", total=711)
+        balance4 = Balance(owner="Json", total=123)
+        balance5 = Balance(owner="Mary", total=417.9)
+        session.add(balance1)
+        session.add(balance2)
+        session.add(balance3)
+        session.add(balance4)
+        session.add(balance5)
+
         balances = session.scalars(select(Balance)).all()
+
         return [{
             "id": balance.id,
             "owner": balance.owner,
